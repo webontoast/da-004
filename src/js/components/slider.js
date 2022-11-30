8250b9ad4b359457c2fc9a0736975de4732a8524
Web Ontoast <dariomarquezv@gmail.com>
2022-11-30 09:25:16 +0000

    (js) added flickity slider

---
+++ src/js/components/slider.js
@@ -0,0 +1,9 @@
+var slider = document.querySelector('.main-carousel');
+var flkty = new Flickity( slider, {
+    // options
+    autoplay: 3000,
+    // pauseAutoPlayOnHover: false,
+    prevNextButtons: false,
+    fade: true,
+    // pageDots: false
+});
